fa21f6dbd5b0b3db23012de3834b7ed8dcf2b000
mike <mike@mikeulas.(none)>
2008-07-23 00:13:07 -0400

    squarez

--- lib/life.rb
+++ lib/life.rb
@@ -6,22 +6,29 @@ require 'lib/life/board'
 require 'lib/life/cell'
 
 module Life
-  mattr_reader :board
+  mattr_reader :board, :background, :screen, :queue, :clock
   
   class << self
     def start!
-      Rubygame.init()
-      screen        = Rubygame::Screen.new([800, 600])
-      screen.title  = "Conway's Game of Life"
-      queue         = Rubygame::EventQueue.new
-      clock         = Rubygame::Clock.new { |clock| clock.target_framerate = 30 }
-      background    = Rubygame::Surface.new(screen.size)
-      background.fill([0,0,0])
-      background.blit(screen, [0,0])
-      screen.update()
+      @@board         = Board.new(100,40)
       
-      loop do
-        puts "tick! #{Time.now.to_s}"
+      Rubygame.init()
+      @@screen        = Rubygame::Screen.new([board.width*7, board.height*7])
+      @@screen.title  = "Conway's Game of Life"
+      @@queue         = Rubygame::EventQueue.new
+      @@clock         = Rubygame::Clock.new { |clock| clock.target_framerate = 30 }
+      @@background    = Rubygame::Surface.new(screen.size)
+      
+      background.fill([0,0,0])
+
+      quit = false
+      
+      until quit do
+        board.calculate!
+        render_board!
+        background.blit(screen, [0,0])
+        screen.title = "Conway's Life (#{clock.framerate.round} fps)"
+        screen.update()
         clock.tick()
         
         quit = false
@@ -43,18 +50,31 @@ module Life
           #   quit = true
           end
         end # event handling
-        
-        break if quit
-        
-        background.blit(screen, [0, 0])
-        screen.update()
-        screen.title = "framerate: #{clock.framerate.to_s}"
-
       end # main loop
       puts "terminated!"
       Rubygame.quit()
       
     end # start!()
+    
+    def render_board!
+      (0..board.height-1).each do |y|
+        (0..board.width-1).each do |x|
+          cell    = board.columns[x][y]
+          color   = (cell.alive? ? [255,255,255] : [0,0,0])
+      
+          # for circle
+          # center  = [cell.x*4, cell.y*4]
+          # radius  = 2
+          # background.draw_circle_s(center, radius, color)
+          
+          # for square
+          p1 = [(cell.x*7)-3, (cell.y*7)-3]
+          p2 = [(cell.x*7)+3, (cell.y*7)+3]
+          background.draw_box_s(p1, p2, color)
+        end
+      end
+    end
+
   end # class methods
   
 end
